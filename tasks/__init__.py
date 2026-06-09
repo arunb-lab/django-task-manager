9ebec503c3ad2275ebe53eed8427a1cb99b2a26d
Arun Baral <baralbhuwan6@gmail.com>
2026-06-09 18:15:42 +0545

    Add tasks/__init__.py
    This file indicates that the 'tasks' directory is a Python package.

---
+++ tasks/__init__.py
@@ -0,0 +1,4 @@
+# tasks/__init__.py
+# This file is intentionally empty.
+# Its presence tells Python that the 'tasks' folder is a Python package.
+# See taskmanager/__init__.py for a longer explanation.
