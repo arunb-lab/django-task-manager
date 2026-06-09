6913c34334deb71bfb2d9553b6ecff4862e43e5e
Arun Baral <baralbhuwan6@gmail.com>
2026-06-09 18:07:14 +0545

    Add taskmanager/__init__.py
    Add __init__.py to define taskmanager as a package

---
+++ taskmanager/__init__.py
@@ -0,0 +1,13 @@
+# taskmanager/__init__.py
+#
+# This file is intentionally (mostly) empty.
+#
+# In Python, any folder with an __init__.py file is called a "package".
+# This file tells Python: "treat this folder as a package you can import from."
+#
+# Example: Because of this file, you can write:
+#   from taskmanager import settings
+#   from taskmanager.urls import urlpatterns
+#
+# Without __init__.py, Python wouldn't recognize 'taskmanager' as a package.
+# Django's 'startproject' command creates this file automatically.
